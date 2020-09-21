fce1f35b86adb869ae8cc130152479f96d0a430f
Kyle <kh09211@georgiasouthern.edu>
2020-09-21 16:35:18 -0500

    updated resume info

--- src/components/Resume.vue
+++ src/components/Resume.vue
@@ -6,37 +6,49 @@
 		<div class="container">
 			<div class="row justify-content-center">
 				<div class="col-md-10  offset-md-1 text-left">
-					<ul><b>Back End Development</b>
-						<li>PHP 7 best practices and design principles, Object Orientated Programming, and use of Composer</li>
-						<li>Laravel framework for PHP and use of Artisan, Blade templating, Eloquent models, Tinker, database 
-					<br>migrations user authentication, authorization, routes, controllers, webpack, and caching with Reddis</li>
-						<li>MySQL and SQLite database migrations, queries, security, and CRUD use with ORMs, PDO, and API</li>
+
+					<ul><b>General Programming</b>
+						<li>Object oriented (OOP), functional, and procedural programming principles and design practices</li>
+						<li>Understanding of Models, Views, Controllers, MVC and MVVM frameworks, and state management</li>
+						<li>Algorithm scripting, data structures, regular expressions, code debugging and refactoring</li>
 						<li>GIT/Git-Hub versioning software, initializing repositories, cloning, forking, commits, revert versions</li>
-						<li>Code debugging, parsing data, form validation, filesystem manipulation, and JSON/XML formating</li>
-						<li>Payment integration using Stripe API, Laravel Cashier, WooCommerce, and managing Wordpress</li>
+						<li>Self-taught and capable of quickly learning new languages, technologies, and programming styles</li>
 					</ul>
-					<ul><b>Front End Development</b>
-						<li>Javascript (ES6) Object Oriented Programming and app design principles, algorithm scripting and
-					<br> data structures, regular expressions, debugging, AJAX calls, Axios, jQuery, and custom carousels</li>
-						<li>Vue.js framework for Javascript and the use of Vuex, Vue-CLI, Vue Router, NPM, and Node.js</li>
-						<li>Experience with Zeplin, Figma, and turning UI/UX designs into functional pixel perfect front ends</li>
+
+					<ul><b>Back End Technologies</b>
+						<li>PHP 7 best practices, use of Composer, class autoloaders, class inheritance, traits, and type hinting</li>
+						<li>Laravel framework for PHP and use of artisan, blade templating, eloquent ORM, tinker, database 
+					<br>migrations, user authentication, authorization, routes, resource controllers, caching and Redis</li>
+						<li>REST API and Microservices using LUMEN framework for PHP and Express.js framework for Node</li>
+						<li>MYSQL and SQLite queries, database migrations, and CRUD use with models, ORMs, and APIs</li>
+						<li>Middlewares, parsing data, form validation, CSRF tokens, filesystem manipulation, and JSON/XML</li>
+						<li>Payment integrations using Stripe, Paypal, Laravel Cashier, WooCommerce, and Wordpress</li>
+					</ul>
+					<ul><b>Front End Technologies</b>
+						<li>Javascript (ES6),  API/AJAX calls using Axios and jQuery, custom carousels, modals, and pop-ups</li>
+						<li>Single Page Applications (SPAs) as static websites and integration of back end APIs for CRUD ops</li>
+						<li>Vue.js framework for Javascript and the use of VueX, Vue-CLI, Vue Router, and authorization gates</li>
 						<li>HTML 5 markup, visual design, applied accessibility, and mobile first website design principles</li>
-						<li>CSS 3 and Bootstrap 4 proficiency, CSS Flexbox, CSS Grid, SASS, and mobile responsive media queries</li>
+						<li>Bootstrap 4, CSS 3, CSS Flexbox, CSS Grid, SASS, media queries, CSS animations and transitions</li>
+						<li>Experience with Zeplin, Figma, and turning UI/UX designs into functional pixel perfect front ends</li>
 					</ul>
 					<ul><b>System Administration</b>
-						<li>Deployment  of  application code to servers, setting up databases, caching, special configurations</li>
-						<li>Proficiency with the shell command line, SSH, FTP, shell scripting, and custom kernel compliation</li>
-						<li>Ubuntu server, Red-hat server, Arch distributions, package management, Postfix mail MTA and config</li>
+						<li>Proficiency with the shell command line, SSH, FTP,  shell scripting, and custom kernel compilation</li>
+						<li>Deployment of application code to servers, setting up databases, caching, special configurations</li>
+						<li>Ubuntu server, Red-hat server, Arch distributions, package management, setup of SMTP mail servers</li>
 						<li>AWS cloud management, EC2 instances, Route 53 and hosted zone DNS name record configurations</li>
-						<li>Apache2 and NGINX web server installation and configuration of virtual servers, and related modules</li>
-						<li>Maintain security through routine updates, upgrades, patches, and SSL / TLS certificate installations</li>
+						<li>Apache2 and NGINX web server setup, virtual servers, reverse proxies, and related config/modules</li>
+						<li>Maintain security through SSL certificate installation, routine updates, upgrades, and patches</li>
+						<li>Migration of websites and databases between hosting companies, webservers, and domains</li>
 					</ul>
 					<ul><b>Other Relevant Skills/Certs</b>
-						<li>FreeCodeCamp.org (300hr) Responsive Web Design Certification (2019)</li>
-						<li>Experience with Matlab, Basic, Visual Basic, C Complers, Decompilers, IDEC PLC Controllers and HMIs</li>
-						<li>Training and experience with LEAN Six Sigma, line balancing, efficiency, and capacity</li>
+						<li>Responsive Web Design Certification (300 hours) - FreeCodeCamp.org</li>
+						<li>Javascript Algorithms and Data Structures Certification (300 hours) - FreeCodeCamp.org</li>
+						<li>Experience with Matlab, Basic, Visual Basic, C, Compilers and Decompilers</li>
+						<li>ASE Automotive Certifications - Electrical Systems / Engine Repair</li>
+						<li>Spanish at the B1 level and TEFL teaching certification from Full-Circle</li>
 						<li>Custom wiring harnesses, relay and controller boards, Megasquirt PCM, and standalone circuitry</li>
-						<li>Fine tuning of electronic fuel injection and ignition systems for racing application</li>
+						<li>Fine tuning of electronic fuel injection and ignition systems for racing applications</li>
 					</ul>
 				</div>
 			</div>
